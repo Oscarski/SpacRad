36b7a70e554ea47581a3464272c7e8905b9a0990
8molik <74592649+8molik@users.noreply.github.com>
2020-12-18 09:15:43 +0100

    Update jasnosc.py
    Obliczanie jasności oraz tworzenie pliku csv

--- jasnosc.py
+++ jasnosc.py
@@ -1,24 +1,45 @@
-import sys
+from logzero import logger, logfile
 from PIL import Image
 import os
 
+minimum_brightness = 0.35 #minimalna jasnosc
+photo_amount = 6 #ilosc zdjec
+
+# czyta i zapisuje sciezke tam gdzie jest main.py
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# tworzy logfile
+logfile(dir_path + "/spacerad.csv")
+
+
 def calculate_brightness(image):
-    greyscale_image = image.convert('L')
-    histogram = greyscale_image.histogram()
+    greyscale = image.convert('L')
+    histogram = greyscale.histogram()
     pixels = sum(histogram)
     brightness = scale = len(histogram)
 
     for index in range(0, scale):
         ratio = histogram[index] / pixels
-        brightness += ratio * (-scale + index)
+        brightness += ratio * (index - scale)
+    if brightness == 255:
+        return 1
+    else:
+        return brightness / scale
 
-    return 1 if brightness == 255 else brightness / scale
+logger.info("Mission started") #poczatek misji
 
-i = 0
-while i <= 7:#w zaleznosci od ilosci zdjec
-    i += 1
-    image = Image.open("P ({}).jpg".format(i))
-    jasnosc = calculate_brightness(image)
-    print(jasnosc)
-    if jasnosc < 0.1:
-        os.remove("P ({}).jpg".format(i))
+index = 0
+
+while index <= photo_amount:#w zaleznosci od ilosci zdjec
+    try:
+        index += 1
+        image = Image.open("P{}.jpg".format(index))
+        jasnosc = calculate_brightness(image)
+        print("Zdjecie {}:".format(index), jasnosc)
+        if jasnosc < minimum_brightness:
+            os.remove("P{}.jpg".format(index))
+    except Exception as e:
+            logger.error("{}: {})".format(e.__class__.__name__, e))
+            logger.info("\tNie ma pliku, lub problem z jasnoscia")
+
+
